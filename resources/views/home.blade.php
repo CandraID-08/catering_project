ef7155cdea714c71bd6dd84142157fd2e84935d4
MutiaraUtami <utamimutiara20@gmail.com>
2025-10-30 16:44:18 +0700

    tes merge

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Booking Catering</title>
+    </head>
+    <body class="antialiased">
+                <h1 class="text-4xl font-bold text-gray-900 dark:text-white">Welcome to Dapur Ibu!</h1>
+                <p class="mt-4 text-gray-600 dark:text-gray-400">Silahkan pilih paket dan booking sekarang.</p>
+            </div>
+        </div>
+    </body>
+</html>
